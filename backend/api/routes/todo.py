397b0d9fa5af7253691c143b1239ed3dfa6b3e69
jameszaman <james.hedayet@gmail.com>
2023-06-21 21:53:20 +0600

    Added a message for copyright

--- backend/api/routes/todo.py
+++ backend/api/routes/todo.py
@@ -1,3 +1,10 @@
+"""
+Copyright (c) 2023 James Hedayet Zaman
+All rights reserved.
+This code is the intellectual property of James Hedayet Zaman.
+Unauthorized use, reproduction, or distribution is strictly prohibited.
+For inquiries, please contact james.hedayet@gmail.com.
+"""
 from fastapi import APIRouter, Body
 from fastapi.encoders import jsonable_encoder
 from config import API_PREFIX
