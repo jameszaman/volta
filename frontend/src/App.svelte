4a0460c95e2a4f18c1144fd70744c2824c362840
James Zaman <james.hedayet@gmail.com>
2023-10-13 19:49:54 +0600

    Added tailwindCSs to the project.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,9 +2,12 @@
   import TopNav from "./lib/layout/TopNav.svelte";
   import SideNav from "./lib/layout/SideNav.svelte";
   import Base from "./lib/layout/Base.svelte";
+  import TailwindCss from "./TailwindCSS.svelte";
 
   let current_project = 0
 </script>
+
+<TailwindCss />
 
 <main>
     <TopNav />
